5203ea672ae91c67607fab7a066b41e7334a7ce0
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-02-24 15:49:42 -0500

    Add, remove, clear, and filter added

--- tasklist/app.js
+++ tasklist/app.js
@@ -12,7 +12,12 @@ LoadEventListeners()
 function LoadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask)
-
+    // Remove task event
+    taskList.addEventListener('click', removeTask)
+    // Clear task event
+    clearBtn.addEventListener('click', clearTask)
+    // Filter task
+    filter.addEventListener('keyup', filterTask)
 }
 
 // Add task
@@ -21,5 +26,59 @@ function addTask(e) {
         alert('Add a task')
     }
 
+    // Create li element
+    const li = document.createElement('li')
+    // Add class
+    li.className = 'collection-item'
+    // Text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value))
+    // New link element
+    const link = document.createElement('a')
+    // Add class
+    link.className = 'delete-item secondary-content'
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // Append the link to li
+    li.appendChild(link)
+
+    // Append li to ul
+    taskList.appendChild(li)
+
+    // Clear input
+    taskInput.value = ''
+
     e.preventDefault()
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {
+        e.target.parentElement.parentElement.remove()
+        }
+    }
+}
+
+// Clear task
+function clearTask() {
+    // taskList.innerHTML = ''
+
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild)
+    }
+
+}
+
+// Filter Tasks
+function filterTask(e) {
+    const text = e.target.value.toLowerCase()
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block'
+        } else {
+            task.style.display = 'none'
+        }
+    })
 }
