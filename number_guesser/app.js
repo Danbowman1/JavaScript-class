a01ee56c8915dc6fa3a5f21781f818366ce87bf3
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-05-04 13:34:17 -0400

    added loosing functionality and restart game

--- number_guesser/app.js
+++ number_guesser/app.js
@@ -10,8 +10,8 @@ GAME FUNCTION:
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
-    guessLeft = 3;
+    winningNum = getRandomNum(min, max),
+    guessesLeft = 3;
 
 // UI Elements
 const game = document.querySelector('#game'),
@@ -25,6 +25,13 @@ const game = document.querySelector('#game'),
 minNum.textContent = min
 maxNum.textContent = max
 
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload()
+    }
+} )
+
 // Listen for guess
 guessBtn.addEventListener('click', function(){
     let guess = parseInt(guessInput.value)
@@ -36,16 +43,56 @@ guessBtn.addEventListener('click', function(){
 
     // Check if won
     if(guess === winningNum){
-        // Disable input
-        guessInput.disabled = true
-        // Change border color
-        guessInput.style.borderColor = 'green'
-        // Set Message
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green')
+        // Game over - won
+        gameOver(true, `${winningNum} is correct, YOU WIN!`)
     } else {
+        // Wrong number
+        guessesLeft -= 1
 
+        if(guessesLeft === 0){
+            // Game over - lost
+            gameOver(false, `Game over, you lost. The correct number was ${winningNum}`)
+        } else {
+            // Game continues - answer wrong
+
+            // Change border color
+            guessInput.style.borderColor = 'red'
+
+            // Clear input
+            guessInput.value = ''
+            
+            // Tell user its the wrong number
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red')
+            
+        }
+        
     }
 })
+
+// Game over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red'
+
+    // Disable input
+    guessInput.disabled = true
+    // Change border color
+    guessInput.style.borderColor = color
+    // Set text color
+    message.style.color = color
+    // Set Message
+    setMessage(msg)
+
+    // Play again
+    guessBtn.value = 'Play Again'
+    guessBtn.className += 'play-again'
+    
+}
+
+// Get Winning Number
+function getRandomNum(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
 
 // Set Message
 function setMessage(msg, color){
